6436b8ed1b3d61b9e2931e466704d59b7bef90e0
Gordon Hui <randomizer@macbook-pro.lan>
2021-11-11 19:50:23 -0500

    added material UI and cards for projects

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,87 @@
 import React from 'react';
+import { Grid, Card, CardContent, Typography, TextField, Button } from '@mui/material';
 
 const Contact = () => {
+
+  function onSubmit () {
+    console.log('submitted')
+  }
   return (
-    <h1>this is the contact form</h1>
-  )
+  <Grid className="contactForm" id="contactForm">
+      <Card style={{ maxWidth: 950, padding: '20px 5px', margin: '10px auto' }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            Contact Me
+          </Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            gutterBottom
+          >
+            Comments, Advice or Just want to say Hi, please reach out.
+          </Typography>
+          <form>
+            <Grid container spacing={1}>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  className="form-control firstName"
+                  placeholder="Enter first name"
+                  label="First Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  className="form-control lastName"
+                  placeholder="Enter last name"
+                  label="Last Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  className="form-control email"
+                  placeholder="Enter email"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  className="form-control formInput"
+                  placeholder="Type your message here"
+                  multiline
+                  rows={4}
+                  label="Message"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={onSubmit}
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
 }
 
 export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,62 +4,93 @@ import HateIt from '../images/Project/H-Ate-It.png'
 import NTheLoop from '../images/Project/In the Loop with Drawer.png'
 import Devaltio from '../images/Project/devaltio.png'
 import After from '../images/Project/After.png'
-import styled from 'styled-components';
-
-//styling 
-const ProjectImages = styled.img`
-width: 25%;
-height: 25%;
-`
+import { Container, Grid, Button, CardMedia, Typography, CardContent, CardActions, Card, Link } from '@mui/material';
 
 
 const Projects = () => {
   return (
-    <div className="projectSection" id="projectSection">
+      <Container style={{ maxWidth: '80%', padding: '20px', margin: '10px auto' }}>
+        
+        <Grid container spacing={4} gap={2} align="center">
 
-      <div className="portfolio">
-        <a href="devaltio" className="portfolio_item">
-          <ProjectImages
-            src={Devaltio}
-            alt="devaltio-open-source-product"
-            className="portfolio__img"
-          />
-        </a>
-        <button>Source Code</button> <button>See Live</button>
-      </div>
+          <Grid xs={12} sm={6}  md={4}>
+            <Card sx={{height:'100%', display: 'flex', flexDirection: 'column'}}>
+              <CardMedia component='img' sx={{p:'3%'}} image={Devaltio} alt='hateit homepage' height="95%" width="95%"/>
+              <CardContent align="center">
+              <Typography gutterBottom variant='h5' component='h2'>deValtio</Typography>
+              <Typography>Development Tool | Tracker of Good Food</Typography>
+              <Typography>React | Typescript | Chrome API | Valtio</Typography>
+              </CardContent>
+              <CardActions>
+              <Link href="http://www.devaltio.io/" target="_blank"><Button variant="contained" size='small'>Live</Button></Link>
+                <Link href="https://github.com/oslabs-beta/deValtio" target="_blank"><Button variant="contained" size='small'>Source Code</Button></Link>
+              </CardActions>
+            </Card>
+          </Grid>
 
-      <div className="portfolio">
-        <a href="pupperly" className="portfolio_item">
-          <ProjectImages
-            src={Pupperly}
-            alt="pupperly"
-            className="portfolio__img"
-          />
-        </a>
-        <button>Source Code</button> <button>See Live</button>
-      </div>
+          <Grid xs={12} sm={6}  md={4}>
+            <Card sx={{height:'100%', display: 'flex', flexDirection: 'column'}}>
+              <CardMedia component='img' sx={{p:'3%'}} image={Pupperly} alt='pupperly homepage' height="95%" width="95%"/>
+              <CardContent align='center'>
+              <Typography gutterBottom variant='h5' component='h2'>Pupperly</Typography>
+              <Typography>Web Application | Veterinary Medical Record Aggregator</Typography>
+              <Typography>NextJS | Material UI | Hasura | Netlify</Typography>
+              </CardContent>
+              <CardActions>
+              <Link href="https://pupperly.netlify.app/" target="_blank"><Button variant="contained" size='small' onclick="window.location.href='https://pupperly.netlify.app/';">Live</Button></Link>
+              <Link href="https://github.com/PupperlyDevTeam/pupperly" target="_blank"><Button variant="contained" size='small'>Source Code</Button></Link>
+              </CardActions>
+            </Card>
+          </Grid>
 
-      <div className="portfolio">
-        <a href="intheloop" className="portfolio_item">
-          <ProjectImages src={NTheLoop} alt="in-the-loop" className="portfolio__img" />
-        </a>
-        <button>Source Code</button>
-      </div>
+          <Grid xs={12} sm={6}  md={4}>
+            <Card sx={{height:'100%', display: 'flex', flexDirection: 'column'}}>
+              <CardMedia component='img' sx={{p:'3%'}} image={NTheLoop} alt='in the loop homepage' height="95%" width="95%"/>
+              <CardContent align='center'>
+              <Typography gutterBottom variant='h5' component='h2'>In The Loop</Typography>
+              <Typography>Web Application | Find live music nearby and choose by ear</Typography>
+              <Typography>React | Chakra UI | MongoDB</Typography>
+              </CardContent>
+              <CardActions>
+              <Link><Button variant="contained" size='small' disabled='true'>Live</Button></Link>
+              <Link href="https://github.com/in-the-loop-iteration/scratch-concert-finder"><Button variant="contained" size='small'>Source Code</Button></Link>
+              </CardActions>
+            </Card>
+          </Grid>
 
-      <div className="portfolio">
-        <a href="after" className="portfolio_item">
-          <ProjectImages src={After} alt="after" className="portfolio__img" />
-        </a>
-        <button>Source Code</button>
-      </div>
+          <Grid xs={12} sm={6}  md={4}>
+            <Card sx={{height:'100%', display: 'flex', flexDirection: 'column'}}>
+              <CardMedia component='img' sx={{p:'3%'}} image={After} alt='after homepage' height="95%" width="95%"/>
+              <CardContent align='center'>
+              <Typography gutterBottom variant='h5' component='h2'>After</Typography>
+              <Typography>Web Application | Plan funeral and after death experience</Typography>
+              <Typography>React | Node | PostgreSQL</Typography>
+              </CardContent>
+              <CardActions>
+              <Link><Button variant="contained" size='small' disabled='true'>Live</Button></Link>
+              <Link href="https://github.com/AfterDevTeam/After"><Button variant="contained" size='small'>Source Code</Button></Link>
+              </CardActions>
+            </Card>
+          </Grid>
 
-      <div className="portfolio">
-        <a href="hateit" className="portfolio_item">
-          <ProjectImages src={HateIt} alt="hate-it" className="portfolio__img" />
-        </a>
-        <button>Source Code</button>
-      </div>
-    </div>
+          <Grid xs={12} sm={6}  md={4}>
+            <Card sx={{height:'100%', display: 'flex', flexDirection: 'column'}}>
+              <CardMedia component='img' sx={{p:'3%'}} image={HateIt} alt='hateit homepage' height="95%" width="95%"/>
+              <CardContent align='center'>
+              <Typography gutterBottom variant='h5' component='h2'>HateIt?</Typography>
+              <Typography>Web Application | Tracker of Good Food</Typography>
+              <Typography>React | Node | MongoDB</Typography>
+              </CardContent>
+              <CardActions>
+              <Link><Button variant="contained" size='small' disabled='true'>Live</Button></Link>
+              <Link href="https://github.com/maddogg612/H-Ate-It"><Button variant="contained" size='small'>Source Code</Button></Link>
+              </CardActions>
+            </Card>
+          </Grid>
+
+        </Grid>
+
+      </Container>
   )
 }
 
